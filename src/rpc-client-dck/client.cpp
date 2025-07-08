a4e3761d5e52b51e997a552613bceee45015ad9a
Gabriel <gabeszopinski123@gmail.com>
2025-07-08 02:48:24 -0500

    got container talking to local host!

--- src/rpc-client-dck/client.cpp
+++ src/rpc-client-dck/client.cpp
@@ -5,7 +5,7 @@
 
 
 int main(int argc, char** argv) {
-    rpc::client client("127.0.0.1", 8080);
+    rpc::client client("host.docker.internal", 8080);
     client.call("printPlease");
     std::cout << "I think something is happening!" << std::endl;
     return 0;
---
+++ tests/wiat.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#define phrase here
+
+void here() {
+    printf("wait a minute!\n");
+}
+
+int main() {
+    phrase();
+    return 0;
+}
