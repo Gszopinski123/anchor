658aabbb5e829b25b817095bc5e5685b7dc26531
Gabriel <gabeszopinski123@gmail.com>
2025-07-10 16:53:26 -0500

    started kubernetes pods

--- src/rpc-client-dck/client.cpp
+++ src/rpc-client-dck/client.cpp
@@ -5,8 +5,9 @@
 
 
 int main(int argc, char** argv) {
-    rpc::client client("host.docker.internal", 8080);
-    client.call("printPlease");
-    std::cout << "I think something is happening!" << std::endl;
+    //rpc::client client("host.docker.internal", 8080);
+    //client.call("printPlease");
+    //std::cout << "I think something is happening!" << std::endl;
+    while(1) {;;}
     return 0;
 }
